621a97cee3b69872c45ec1995b573153a791956c
Olgaa-Z <zelviaolgamaharani98@gmail.com>
2023-07-07 16:02:24 +0700

    Initial Commit

---
+++ WebView-Ios/WebView_IosApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_IosApp.swift
+//  WebView-Ios
+//
+//  Created by visionet on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct WebView_IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
